5c93632f0a59c6b1516acdc34fe825d10fc109de
Nokeen <cordrepayne@gmail.com>
2022-11-19 10:57:22 -0600

    Created random rock,paper,scissors generator

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String[] choices = { "rock", "paper", "scissors" };
+        int test = (int)Math.round(Math.random()*2);
+        System.out.println(choices[test]);
     }
 }
