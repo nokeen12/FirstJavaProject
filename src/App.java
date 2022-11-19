fc939e64bfc8c2922a2327cc0c0fde5bcacc4364
Nokeen <cordrepayne@gmail.com>
2022-11-19 11:35:19 -0600

    Imported Scanner util and created working rock paper scissors game

--- src/App.java
+++ src/App.java
@@ -1,7 +1,33 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         String[] choices = { "rock", "paper", "scissors" };
-        int test = (int)Math.round(Math.random()*2);
-        System.out.println(choices[test]);
+        String value;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Do you want to play rock, paper, scissors? (Y/N): ");
+        value = scanner.nextLine();
+        System.out.println("You chose: " + value);
+        if(value.equals("Y") || value.equals("y")){
+            int test = (int)Math.round(Math.random()*2);
+            System.out.print("Choose one [rock, paper, scissors]: ");
+            value = scanner.nextLine().toLowerCase();
+            scanner.close();
+            System.out.println("Opponent chose: " + choices[test]);
+            if((value.equals("rock") && choices[test].equals("paper")) || (value.equals("paper") && choices[test].equals("scissors")) || (value.equals("scissors") && choices[test].equals("rock"))){
+                //lose
+                System.out.println("You Lose...");
+            }else if((value.equals("paper") && choices[test].equals("rock")) || (value.equals("scissors") && choices[test].equals("paper")) || (value.equals("rock") && choices[test].equals("scissors"))){
+                //win
+                System.out.println("You Win!!!");
+            }else if(value.equals(choices[test])){
+                //tie
+                System.out.println("You Tied.");
+            }else{
+                //error
+                System.out.println("Invalid choice!");
+            }
+        };
+        System.out.println("Game Over...");
     }
 }
